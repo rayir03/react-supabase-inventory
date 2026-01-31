293fcd3c94ec5380247f73eaa3af7065624f14e2
rayir03 <rayir03@gmail.com>
2026-01-30 18:56:43 -0600

    barcode1

---
+++ src/components/BarcodeScanner.jsx
@@ -0,0 +1,18 @@
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+
+// export default function BarcodeScanner({ onScan }) {
+// //   return (
+// //     <BarcodeScannerComponent
+// //       width={320}
+// //       height={320}
+// //       onUpdate={(err, result) => {
+// //         if (result) {
+// //           onScan(result.text);
+// //         }
+// //       }}
+// //     />
+// //   );
+// }
+export default function BarcodeScanner() {
+  return <h2>Scanner cargado</h2>;
+}
